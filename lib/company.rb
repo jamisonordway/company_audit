3928681ea044ba61fba04cf49865a65c979b56f7
JamisonOrdway <jamisonordway@gmail.com>
2018-04-17 10:38:01 -0600

    Iteration 2 WIP

---
+++ lib/company.rb
@@ -0,0 +1,16 @@
+
+class Company
+  attr_reader :employees,
+              :projects,
+              :timesheets
+  def initialize
+    @employees = []
+    @projects = []
+    @timesheets = []
+  end
+
+  def load_employees(filename)
+    load(filename)
+    binding.pry
+  end
+end
---
+++ test/company_test.rb
@@ -0,0 +1,25 @@
+require './test/test_helper'
+require './lib/company'
+require 'date'
+require 'pry'
+
+class CompanyTest < Minitest::Test
+
+  def setup
+    @company = Company.new
+  end
+
+  def test_it_exists
+    assert_instance_of Company, @company
+  end
+
+  def test_it_starts_with_empty_attributes
+    assert_equal [], @company.employees
+    assert_equal [], @company.projects
+    assert_equal [], @company.timesheets
+  end
+
+  def test_it_can_load_employees
+    assert_instance_of Array, @company.load_employees('./data/employees.csv').split(",")
+  end
+end
