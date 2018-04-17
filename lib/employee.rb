362b936fe8d90c267a5bfb4e5d0b070cf968b0e4
JamisonOrdway <jamisonordway@gmail.com>
2018-04-17 09:31:11 -0600

    Iteration 1 WIP

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,23 @@
+
 class Employee
+  attr_reader :id,
+              :name,
+              :role
+
+  def initialize(id, name, role, start_date, end_date)
+    @id = id.to_i
+    @name = name
+    @role = role
+    @start_date = end_date
+    @end_date = end_date
+  end
+
+  def start_date(start_date)
+    DateHandler.string_to_date(start_date)
+  end
+
+  def end_date(end_date)
+    DateHandler.string_to_date(end_date)
+  end
+
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -1,10 +1,23 @@
 require './test/test_helper'
 require './lib/employee'
 
+
 class EmployeeTest < Minitest::Test
+
+  def setup
+    @employee = Employee.new('5', 'Sally Jackson',
+    'Engineer', '2015-01-01', '2018-01-01')
+  end
+
   def test_instantiation
+    assert_instance_of Employee, @employee
   end
 
   def test_attributes
+    assert_equal Integer, @employee.id.class
+    assert_equal 'Sally Jackson', @employee.name
+    assert_equal 'Engineer', @employee.role
+    assert_equal Date, @employee.start_date.class
+    assert_equal Date, @employee.end_date.class
   end
 end
