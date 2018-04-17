e8a5c7e6b6c727244af7395177ddcd145072cfa7
JamisonOrdway <jamisonordway@gmail.com>
2018-04-17 09:40:34 -0600

    Iteration 1 WIP

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -1,5 +1,8 @@
 require './test/test_helper'
 require './lib/employee'
+require './modules/date_handler'
+require 'date'
+
 
 
 class EmployeeTest < Minitest::Test
@@ -7,6 +10,7 @@ class EmployeeTest < Minitest::Test
   def setup
     @employee = Employee.new('5', 'Sally Jackson',
     'Engineer', '2015-01-01', '2018-01-01')
+    @dh = DateHandler::DHDate.new(date)
   end
 
   def test_instantiation
@@ -17,7 +21,7 @@ class EmployeeTest < Minitest::Test
     assert_equal Integer, @employee.id.class
     assert_equal 'Sally Jackson', @employee.name
     assert_equal 'Engineer', @employee.role
-    assert_equal Date, @employee.start_date.class
-    assert_equal Date, @employee.end_date.class
+    assert_instance_of Date, @employee.start_date('2015-01-01')
+    assert_instance_of Date, @employee.end_date('2018-01-01')
   end
 end
